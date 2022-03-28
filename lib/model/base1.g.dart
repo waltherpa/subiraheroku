643157d5d7abf8b69cf0df921602c252743b653b
Walther Rojas Llerena <wrojas@panaautos.com.pe>
2022-03-27 19:30:04 -0500

    new models

---
+++ lib/model/base1.g.dart
@@ -0,0 +1,39 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'base1.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Base1 _$Base1FromJson(Map<String, dynamic> json) => Base1(
+      json['id'] as int,
+      json['NROMOTOR_VEH'] as String?,
+      json['CHASIS_VEH'] as String?,
+      json['CLIENTE'] as String?,
+      json['EMAIL'] as String?,
+      json['PERSONA_CONTACTO'] as String?,
+      json['COD_CLIENTE'] as String?,
+      json['PLACA_VEH_TARJETA'] as String?,
+      json['VERSION_MODELO'] as String?,
+      json['COLOR'] as String?,
+      json['SERIE'] as String?,
+      json['TELEFONO1'] as String?,
+      json['BASE'] as String?,
+    );
+
+Map<String, dynamic> _$Base1ToJson(Base1 instance) => <String, dynamic>{
+      'id': instance.id,
+      'NROMOTOR_VEH': instance.NROMOTOR_VEH,
+      'CHASIS_VEH': instance.CHASIS_VEH,
+      'CLIENTE': instance.CLIENTE,
+      'EMAIL': instance.EMAIL,
+      'PERSONA_CONTACTO': instance.PERSONA_CONTACTO,
+      'COD_CLIENTE': instance.COD_CLIENTE,
+      'PLACA_VEH_TARJETA': instance.PLACA_VEH_TARJETA,
+      'VERSION_MODELO': instance.VERSION_MODELO,
+      'COLOR': instance.COLOR,
+      'SERIE': instance.SERIE,
+      'TELEFONO1': instance.TELEFONO1,
+      'BASE': instance.BASE,
+    };
---
+++ lib/model/logcitas.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'logcitas.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+LogCitas _$LogCitasFromJson(Map<String, dynamic> json) => LogCitas(
+      json['id_lg'] as int,
+      json['Usuario'] as String,
+      json['Fecha'] as String,
+      json['Hora'] as String,
+      json['Placa'] as String,
+      json['Modelo'] as String,
+      json['Nveh'] as String,
+      json['Nombre'] as String,
+      json['Documento'] as String,
+      json['Correo'] as String,
+      json['Telefono'] as String,
+      json['TipoProblema'] as String,
+      json['SuptipoProblema'] as String,
+      json['TipoLlamada'] as String,
+      json['Sede'] as String,
+      json['EstadoCita'] as String,
+      json['Comentarios'] as String,
+    );
+
+Map<String, dynamic> _$LogCitasToJson(LogCitas instance) => <String, dynamic>{
+      'id_lg': instance.id_lg,
+      'Usuario': instance.Usuario,
+      'Fecha': instance.Fecha,
+      'Hora': instance.Hora,
+      'Placa': instance.Placa,
+      'Modelo': instance.Modelo,
+      'Nveh': instance.Nveh,
+      'Nombre': instance.Nombre,
+      'Documento': instance.Documento,
+      'Correo': instance.Correo,
+      'Telefono': instance.Telefono,
+      'TipoProblema': instance.TipoProblema,
+      'SuptipoProblema': instance.SuptipoProblema,
+      'TipoLlamada': instance.TipoLlamada,
+      'Sede': instance.Sede,
+      'EstadoCita': instance.EstadoCita,
+      'Comentarios': instance.Comentarios,
+    };
